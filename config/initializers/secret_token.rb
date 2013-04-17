340bf0dcd52a9f3bab211a97e2baf17759209d02
Your Name <heishaeun@gmail.com>
2013-04-17 20:05:00 +0900

    XCXXZ
    :Q
    Xfirst

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heishaeun::Application.config.secret_token = 'c8f4806e8e8481fc4441dbdb92fda93da05bb3bfe1881dbc27e8f84f153b593b8e3b3ac8ac7a42f55e9833c9195d375967007d85771fcfb0565dacff58fe12bb'
